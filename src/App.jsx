dd4aee9b9331ff8e71570a0d0a2fc19a5dca10d4
GUchechi <godswilluchendu7@gmail.com>
2023-05-09 00:05:15 +0100

    Cart

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <Cart />
+      {/* <Cart /> */}
       <main className="flex flex-col gap-16 relative">
         <Hero heroapi={heroapi} />
         <Sales endpoint={popularsales} ifExists />
