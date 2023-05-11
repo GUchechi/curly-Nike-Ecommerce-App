aa0f714870df567636f2bf735fa66e9094438d3b
GUchechi <godswilluchendu7@gmail.com>
2023-05-12 00:55:46 +0100

    CartItem

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -8,7 +8,9 @@ const Cart = () => {
       <div
         className={`fixed top-0 left-0 right-0 bottom-0 blur-effect-theme w-full h-screen opacity-100 z-[250] `}
       >
-        <div>
+        <div
+          className={`blur-effect-theme h-screen max-w-xl w-full absolute right-0`}
+        >
           <CartCount />
           <CartEmpty />
           <CartItem />
