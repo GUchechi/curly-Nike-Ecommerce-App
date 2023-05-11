0368a73314f9ca8494a7d14dcd2c7101431e02cc
GUchechi <godswilluchendu7@gmail.com>
2023-05-12 00:51:04 +0100

    CartItem

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,14 +1,21 @@
 import CartCount from "./cart/CartCount";
 import CartEmpty from "./cart/CartEmpty";
+import CartItem from "./cart/CartItem";
 
 const Cart = () => {
-  return <div>
-    <div>
-      <CartCount />
-      <CartEmpty />
-      <CartIt
-    </div>
-  </div>;
+  return (
+    <>
+      <div
+        className={`fixed top-0 left-0 right-0 bottom-0 blur-effect-theme w-full h-screen opacity-100 z-[200] `}
+      >
+        <div>
+          <CartCount />
+          <CartEmpty />
+          <CartItem />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Cart;
