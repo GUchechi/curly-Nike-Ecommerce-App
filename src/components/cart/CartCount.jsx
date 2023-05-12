0da54e9992693a668a7eb9d5e3cc7ea2229ccc99
GUchechi <godswilluchendu7@gmail.com>
2023-05-12 01:32:49 +0100

    CartCount

--- src/components/cart/CartCount.jsx
+++ src/components/cart/CartCount.jsx
@@ -11,8 +11,8 @@ const CartCount = () => {
           <div className="grid items-center">
             <h1 className="text-base font-medium text-slate-900">
               Your Cart{" "}
-              <span className="bg-theme-cart rounded px-1 py-0.5 text-slate-100 font-normal">
-                (Items)
+              <span className="bg-theme-cart rounded px-1 py-0.5 text-slate-100 font-normal text-xs">
+                (0 Items)
               </span>
             </h1>
           </div>
--- src/components/cart/CartEmpty.jsx
+++ src/components/cart/CartEmpty.jsx
@@ -1,9 +1,22 @@
-
+import emptybag from "../../assets/emptybag.png";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 
 const CartEmpty = () => {
   return (
-    <div>CartEmpty</div>
-  )
-}
+    <>
+      <div className="flex items-center justify-center flex-col h-screen px-11 text-center gap-7">
+        <img
+          src={emptybag}
+          alt="emptybag/img"
+          className="w-40 lg:w-36 sm:w-28 h-auto object-fill transition-all duration-300 hover:scale-110"
+        />
+        <button>
+          <ArrowLeftIcon />
+          <span>Back To Nike</span>
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default CartEmpty
+export default CartEmpty;
