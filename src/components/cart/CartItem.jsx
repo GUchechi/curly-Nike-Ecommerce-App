aee0bf036c03a9bd5fbfd070453c691ad84517ad
GUchechi <godswilluchendu7@gmail.com>
2023-05-13 00:03:21 +0100

    cartItem

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -31,21 +31,31 @@ const CartItem = ({
                 <MinusIcon className="w-5 h-5 lg:w-4 lg:h-4 text-white stroke-[2]" />
               </button>
 
-              <div>{cartQuantity}</div>
+              <div className="bg-theme-cart rounded text-white font-medium lg:text-xs w-7 h-6 lg:h-5 lg:w-6 flex items-center justify-center">
+                {cartQuantity}
+              </div>
 
-              <button type="button" className="">
-                <PlusIcon className="w-5 h-5" />
+              <button
+                type="button"
+                className="bg-theme-cart rounded w-6 h-6 lg:w-5 lg:h-5 flex items-center justify-center active:scale-90"
+              >
+                <PlusIcon className="w-5 h-5 lg:w-4 lg:h-4 text-white stroke-[2]" />
               </button>
             </div>
           </div>
         </div>
-        <div>
-          <div>
-            <h1>{price * cartQuantity}</h1>
+        <div className="grid items-center gap-5">
+          <div className="grid items-center justify-center">
+            <h1 className="text-lg lg:text-base text-slate-900 font-medium">
+              ${price * cartQuantity}
+            </h1>
           </div>
-          <div>
-            <button type="button" className="">
-              <TrashIcon className="w-5 h-5" />
+          <div className="grid items-center justify-center">
+            <button
+              type="button"
+              className="bg-theme-cart rounded p-1 lg:p-0.5 grid items-center justify-items-center"
+            >
+              <TrashIcon className="w-5 h-5 text-white" />
             </button>
           </div>
         </div>
