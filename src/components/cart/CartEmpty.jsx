a75431ebe8097cb427add69dbe3f5982518eb02f
GUchechi <godswilluchendu7@gmail.com>
2023-05-12 01:39:36 +0100

    CartCount

--- src/components/cart/CartEmpty.jsx
+++ src/components/cart/CartEmpty.jsx
@@ -10,7 +10,10 @@ const CartEmpty = () => {
           alt="emptybag/img"
           className="w-40 lg:w-36 sm:w-28 h-auto object-fill transition-all duration-300 hover:scale-110"
         />
-        <button>
+        <button
+          type="button"
+          className="button-theme bg-gradient-to-b from-amber-500 to-orange-500 shadow-lg shadow-orange-500 flex items-center justify-center text-slate-900 py-2 gap-3 text-sm px-5 font-semibold active:scale-110"
+        >
           <ArrowLeftIcon />
           <span>Back To Nike</span>
         </button>
