aa4a997983660e86f12390f3fd9d4a2b63a0be99
GUchechi <godswilluchendu7@gmail.com>
2023-05-12 15:48:55 +0100

    addItems

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,9 +1,44 @@
+import { MinusIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/outline";
+
 const CartItem = ({
   item: { id, title, text, img, color, shadow, price, cartQuantity },
 }) => {
   return (
     <>
-      <div>CartItem</div>
+      <div>
+        <div>
+          <div>
+            <img src={img} alt={`img/cart-item/${id}`} />
+          </div>
+          <div>
+            <div>
+              <h1>{title}</h1>
+              <p>{text}</p>
+            </div>
+            <div>
+              <button type="button">
+                <MinusIcon className="w-5 h-5" />
+              </button>
+
+              <div>{cartQuantity}</div>
+
+              <button type="button" className="">
+                <PlusIcon className="w-5 h-5" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h1>{price * cartQuantity}</h1>
+          </div>
+          <div>
+            <button type="button" className="">
+              <TrashIcon className="w-5 h-5" />
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
