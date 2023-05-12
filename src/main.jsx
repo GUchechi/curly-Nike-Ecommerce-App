6f849de639ad9b5c5ef68910b2756865222aec9d
GUchechi <godswilluchendu7@gmail.com>
2023-05-12 15:11:18 +0100

    toast

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import Store from "./app/Store.js";
+import { Toaster } from "react-hot-toast";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={Store}>
+      <Toaster position="top-center" reverseOrder={false} />
       <App />
     </Provider>
   </React.StrictMode>
