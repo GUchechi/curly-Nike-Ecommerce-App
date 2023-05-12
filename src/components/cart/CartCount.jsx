23886a5caac4b59b579babcf4ba4843bfd19d8b5
GUchechi <godswilluchendu7@gmail.com>
2023-05-12 01:01:40 +0100

    CartItem

--- src/components/cart/CartCount.jsx
+++ src/components/cart/CartCount.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { ChevronDoubleLeftIcon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const CartCount = () => {
   return (
-    <div>CartCount</div>
-  )
-}
+    <>
+      <div>
+        <div>
+          <div>
+            <ChevronDoubleLeftIcon />
+          </div>
+          <div>
+            <h1>
+              Your Cart <span>(Items)</span>
+            </h1>
+          </div>
+        </div>
+        <div>
+          <button type="button">
+            <XMarkIcon />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default CartCount
+export default CartCount;
