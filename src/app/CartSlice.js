dec4da0953904b7a3639e0d54d1283067d7c9566
GUchechi <godswilluchendu7@gmail.com>
2023-05-12 15:02:40 +0100

    items

--- src/app/CartSlice.js
+++ src/app/CartSlice.js
@@ -16,12 +16,22 @@ const CartSlice = createSlice({
       state.cartState = action.payload.cartState;
     },
     setAddItemToCart: (state, action) => {
-      state.cartItems.push(action.payload);
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex >= 0) {
+        state.cartItems[itemIndex].cartQuantity += 1;
+      } else {
+        const temp = { ...action.payload, cartQuantity: 1 };
+        state.cartItems.push(temp);
+      }
     },
   },
 });
 
-export const { setOpenCart, setCloseCart,setAddItemToCart } = CartSlice.actions;
+export const { setOpenCart, setCloseCart, setAddItemToCart } =
+  CartSlice.actions;
 
 export const selectCartState = (state) => state.cart.cartState;
 
