b1499d699176c86aa5bcc42134a88180b278e465
GUchechi <godswilluchendu7@gmail.com>
2023-05-12 01:09:38 +0100

    CartCount

--- src/components/cart/CartCount.jsx
+++ src/components/cart/CartCount.jsx
@@ -4,7 +4,7 @@ const CartCount = () => {
   return (
     <>
       <div className="bg-white h-11 flex items-center justify-between px-3 sticky top-0 left-0 right-0 w-full">
-        <div>
+        <div className="flex items-center gap-3">
           <div>
             <ChevronDoubleLeftIcon className="h-5 w-5" />
           </div>
