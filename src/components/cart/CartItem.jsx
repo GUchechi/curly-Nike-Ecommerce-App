4af6f28769b644eb0e7f431a32358cf5ab6899f4
GUchechi <godswilluchendu7@gmail.com>
2023-05-12 15:55:37 +0100

    addItems

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -8,7 +8,11 @@ const CartItem = ({
       <div>
         <div>
           <div>
-            <img src={img} alt={`img/cart-item/${id}`} />
+            <img
+              src={img}
+              alt={`img/cart-item/${id}`}
+              className="w-36 h-auto object-fill lg:w-28"
+            />
           </div>
           <div>
             <div>
