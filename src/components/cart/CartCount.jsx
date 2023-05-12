035e984956dbff28ca5cb8b5132b6d2f04453733
GUchechi <godswilluchendu7@gmail.com>
2023-05-12 01:12:58 +0100

    CartCount

--- src/components/cart/CartCount.jsx
+++ src/components/cart/CartCount.jsx
@@ -6,7 +6,7 @@ const CartCount = () => {
       <div className="bg-white h-11 flex items-center justify-between px-3 sticky top-0 left-0 right-0 w-full">
         <div className="flex items-center gap-3">
           <div className="grid items-center cursor-pointer">
-            <ChevronDoubleLeftIcon className="h-5 w-5" />
+            <ChevronDoubleLeftIcon className="h-5 w-5 text-slate-900 hover:text-blue-500 stroke-[2]" />
           </div>
           <div>
             <h1>
