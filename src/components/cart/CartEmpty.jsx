eaf8501dc3bc15f0bdda9174b7fad2f98d01910b
GUchechi <godswilluchendu7@gmail.com>
2023-05-12 01:41:19 +0100

    CartEmpty

--- src/components/cart/CartEmpty.jsx
+++ src/components/cart/CartEmpty.jsx
@@ -14,7 +14,7 @@ const CartEmpty = () => {
           type="button"
           className="button-theme bg-gradient-to-b from-amber-500 to-orange-500 shadow-lg shadow-orange-500 flex items-center justify-center text-slate-900 py-2 gap-3 text-sm px-5 font-semibold active:scale-110"
         >
-          <ArrowLeftIcon />
+          <ArrowLeftIcon className="w-5 h-5 text-slate-900" />
           <span>Back To Nike</span>
         </button>
       </div>
