2d4128aa46f43ed8c91ad106da53de2b836a58f6
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 00:05:32 +0100

    refactoring

--- src/app/CartSlice.js
+++ src/app/CartSlice.js
@@ -1,14 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-hot-toast";
 
-const initialState = {
-  cartState: false,
-  cartItems: localStorage.getItem("cart")
-    ? JSON.parse(localStorage.getItem("cart"))
-    : [],
-  cartTotalAmount: 0,
-  cartTotalQuantity: 0,
-};
+
 
 const CartSlice = createSlice({
   initialState,
