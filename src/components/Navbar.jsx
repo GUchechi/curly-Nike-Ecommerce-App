5380b6de7075008fcb3173150d2b13ebb301aa9e
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 00:10:30 +0100

    refactoring

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,13 @@ const Navbar = () => {
     }
   };
 
- 
+  useEffect(() => {
+    window.addEventListener("scroll", onNavScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onNavScroll);
+    };
+  }, []);
 
   return (
     <>
