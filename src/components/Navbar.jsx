b731971adc921f31eac476f591cf42097a10637f
GUchechi <godswilluchendu7@gmail.com>
2023-05-15 00:09:46 +0100

    refactoring

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,13 +13,7 @@ const Navbar = () => {
   const [navState, setNavState] = useState(false);
   const totalQTY = useSelector(selectTotalQTY);
 
-  const onCartToggle = () => {
-    dispatch(
-      setOpenCart({
-        cartState: true,
-      })
-    );
-  };
+ 
 
   const onNavScroll = () => {
     if (window.scrollY > 30) {
